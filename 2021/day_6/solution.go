fea35f0181bab00aaf58189d6301a464a99344ef
c-fandango <edd568@hotmail.co.uk>
2023-05-13 22:45:46 +0100

    day six problem, data and solution

---
+++ 2021/day_6/solution.go
@@ -0,0 +1,73 @@
+// day six solution
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/c-fandango/advent_of_code/2021/utils"
+)
+
+func main() {
+
+	input := utils.ReadDataStr("../data/data_day_6.txt")
+
+	answerOne := partOne(input, 80)
+	answerTwo := partOne(input, 256)
+
+	fmt.Println(answerOne)
+	fmt.Println(answerTwo)
+
+}
+
+func extractData(input string) []int {
+	fishStr := strings.Split(input, ",")
+	fishes := make([]int, len(fishStr))
+
+	for i, fish := range fishStr {
+		fishes[i], _ = strconv.Atoi(fish)
+	}
+	return fishes
+}
+
+func initMap(fishes []int, count int) map[int]int {
+	ages := map[int]int{-1: 0}
+	for i := 0; i <= count; i++ {
+		_, exists := ages[i]
+		if !exists {
+			ages[i] = 0
+		}
+	}
+
+	for _, fish := range fishes {
+		ages[fish]++
+	}
+
+	return ages
+}
+
+func partOne(input []string, days int) int {
+	const childhood, cycleLength = 8, 6
+
+	fishes := extractData(input[0])
+	ages := initMap(fishes, childhood)
+
+	for days > 0 {
+		for i := 0; i <= childhood; i++ {
+			ages[i-1] = ages[i]
+		}
+		ages[childhood] = ages[-1]
+		ages[cycleLength] += ages[-1]
+		ages[-1] = 0
+
+		days--
+	}
+
+	endFishes := 0
+	for _, num := range ages {
+		endFishes += num
+	}
+
+	return endFishes
+}
